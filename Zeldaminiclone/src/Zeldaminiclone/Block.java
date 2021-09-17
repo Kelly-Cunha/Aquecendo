a10ea098ae80e3b3632e313ac5abff859eff9910
Kelly-Cunha <81298551+Kelly-Cunha@users.noreply.github.com>
2021-09-17 05:37:39 -0300

    Em andamento: aula
    Aula

---
+++ Zeldaminiclone/src/Zeldaminiclone/Block.java
@@ -0,0 +1,18 @@
+package Zeldaminiclone;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Block extends Rectangle{
+
+	public Block(int x,int y) {
+		super(x,y,32,32);
+	}
+	
+	public void render(Graphics g) {
+
+		g.drawImage(Spritesheet.tileWall, x,y,32,32,null);
+	}
+	
+}
---
+++ Zeldaminiclone/src/Zeldaminiclone/Game.java
@@ -0,0 +1,143 @@
+package Zeldaminiclone;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable, KeyListener{
+
+	public static int WIDTH = 1370, HEIGHT = 696;
+	public static int SCALE = 3;
+	public static Player player;
+	
+	public World world;
+	public List<Enemy> enemies = new ArrayList<Enemy>();
+	
+	public Game() {
+		this.addKeyListener(this);
+		this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		new Spritesheet();
+		
+		world = new World();
+		
+		player = new Player(32,32);
+		
+		enemies.add(new Enemy(32,32));
+		
+		enemies.add(new Enemy(32,64));
+	}	
+	
+	public void tick() {
+		player.tick();
+		
+		for(int i = 0; i < enemies.size(); i++) {
+			enemies.get(i).tick();
+		}
+	}
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillRect(0, 0, WIDTH*SCALE,HEIGHT*SCALE);
+		
+		player.render(g);
+		for(int i = 0; i < enemies.size(); i++) {
+			enemies.get(i).render(g);
+		}
+		world.render(g);
+
+		bs.show();
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		JFrame frame = new JFrame();
+		
+		frame.add(game);
+		frame.setTitle("Mini Zelda");
+		frame.setResizable(false);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.setVisible(true);
+		
+		new Thread(game).start();
+		
+	}
+	
+	public void run() {
+		
+		while(true) {
+			tick();
+			render();
+			try {
+				Thread.sleep(1000/60);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.right = true;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left = true;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_X) {
+			player.shoot = true;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_UP) {
+			player.up = true;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+			player.down = true;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.right = false;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left = false;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_UP) {
+			player.up = false;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+			player.down = false;
+		}
+		
+	}
+
+}
